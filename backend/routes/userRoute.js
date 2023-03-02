ce7257cab31582ffcd3d642964c5f52aa42c69ca
Kapil Soni <kapilsoni54768161@gmail.com>
2023-03-02 21:33:49 +0530

    Added the routes for user

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { isAuthenticatedUser, authorizeRole } = require("../middleware/auth.js");
-const {loginUser  , registerUser , logout} = require( '../controllers/userController.js')
+const {loginUser  , registerUser , logout , updatePassword , updateProfile , getAllUsers , deleteUser , changeUserRole} = require( '../controllers/userController.js')
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -8,6 +8,19 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
 router.route("/logout").post(logout);
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+
+router.route("/me/updatePassword").put(isAuthenticatedUser, updatePassword);
+router.route("/me/delete/account").put(isAuthenticatedUser, deleteUser);
+
+
+router.route("/admin/users").get(isAuthenticatedUser,authorizeRole("admin"),getAllUsers);
+
+router.route("/admin/delete/user/:id").delete(isAuthenticatedUser,  authorizeRole("admin") , deleteUser)
+
+router.route("/admin/update/role").put(isAuthenticatedUser , authorizeRole("admin") , changeUserRole)
+module.exports = router;
 
 
 module.exports = router;
