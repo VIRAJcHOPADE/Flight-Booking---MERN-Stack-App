0ca3a6e2f9648b748c88bbf25c25269874ff4b5f
Kapil Soni <kapilsoni54768161@gmail.com>
2023-03-05 22:50:30 +0530

    Added authentication to the admin flights page

--- frontend/src/components/AdminDashBoard/FlightHandle/FlightHandle.jsx
+++ frontend/src/components/AdminDashBoard/FlightHandle/FlightHandle.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "../NavBar/AdminNavBar";
-
+import axios from "axios";
+import Loading from "../../Loading/Loading";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 const FlightHandle = () => {
+  const [allFlights, setAllFlights] = useState(null);
+  const [user, setUser] = useState(null);
+  const [role, setRole] = useState("");
+  const navigate = useNavigate();
+
+  const getAllFlights = async () => {
+    const { data } = await axios.get("/api/v1/all/flights");
+    setAllFlights(data);
+  };
+
+  const getUserDetails = async () => {
+    const { data } = await axios.get("/api/v1/me");
+    setUser(data);
+    setRole(data?.user?.role);
+  };
+
+  useEffect(() => {
+    if (role == "user") {
+      toast.error("You are not Authorized !!");
+      navigate("/");
+    }
+    getUserDetails();
+    getAllFlights();
+  }, [role]);
   return (
     <>
-      <AdminNavBar />
-      <div className="content">hii</div>
+      {user != null && user?.success == true && role == "admin" ? (
+        <>
+          <AdminNavBar />
+          <div className="content"></div>
+        </>
+      ) : (
+        <Loading />
+      )}
     </>
   );
 };
