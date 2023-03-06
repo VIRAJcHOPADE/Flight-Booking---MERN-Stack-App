004fe483185337f1ded0f6750f9916ddbede9c09
Kapil Soni <kapilsoni54768161@gmail.com>
2023-03-06 16:02:24 +0530

    Added authorization

--- frontend/src/components/AdminDashBoard/Extras/EventHandle.jsx
+++ frontend/src/components/AdminDashBoard/Extras/EventHandle.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "../NavBar/AdminNavBar";
 import { toast } from "react-toastify";
 import "./extras.scss";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Loading from "../../Loading/Loading";
 const EventHandle = () => {
   const [eventDetail, setEventDetail] = useState({
     name: "",
     destination: "",
     eventPrice: 0,
   });
+  const [user, setUser] = useState(null);
+  const [role, setRole] = useState("");
 
   const createEventHandler = async () => {
     if (eventDetail.name == "" || eventDetail.destination == "") {
@@ -34,56 +38,89 @@ const EventHandle = () => {
       toast.error(data?.message);
     }
   };
+  const navigate = useNavigate();
+
+  const getUserDetails = async () => {
+    const { data } = await axios.get("/api/v1/me");
+    setUser(data);
+    setRole(data?.user?.role);
+  };
+
+  useEffect(() => {
+    getUserDetails();
+  });
+
+  useEffect(() => {
+    if (role == "user") {
+      toast.error("You are not Authorized !!");
+      navigate("/");
+    }
+    getUserDetails();
+  }, [role]);
+
+  useEffect(() => {
+    if (user?.success == false) {
+      toast.error("Unauthorized Access !!");
+      navigate("/");
+    }
+  }, [user]);
 
   return (
     <>
       <AdminNavBar />
+      {user != null && user?.success == true && role == "admin" ? (
+        <>
+          <div className="content create-div-cont color-change">
+            <div className="create-div">
+              <h1>Add Event</h1>
 
-      <div className="content create-div-cont color-change">
-        <div className="create-div">
-          <h1>Add Event</h1>
-
-          <div className="create-form">
-            <div>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                value={eventDetail.name}
-                onChange={(e) => {
-                  setEventDetail({ ...eventDetail, name: e.target.value });
-                }}
-              />
-            </div>
-            <div>
-              <label htmlFor="destination">Destination</label>
-              <input
-                type="text"
-                value={eventDetail.destination}
-                onChange={(e) => {
-                  setEventDetail({
-                    ...eventDetail,
-                    destination: e.target.value,
-                  });
-                }}
-              />
-            </div>
-            <div>
-              <label htmlFor="destination">Price</label>
-              <input
-                type="number"
-                value={eventDetail.eventPrice}
-                onChange={(e) => {
-                  setEventDetail({
-                    ...eventDetail,
-                    eventPrice: e.target.value,
-                  });
-                }}
-              />
+              <div className="create-form">
+                <div>
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    value={eventDetail.name}
+                    onChange={(e) => {
+                      setEventDetail({ ...eventDetail, name: e.target.value });
+                    }}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="destination">Destination</label>
+                  <input
+                    type="text"
+                    value={eventDetail.destination}
+                    onChange={(e) => {
+                      setEventDetail({
+                        ...eventDetail,
+                        destination: e.target.value,
+                      });
+                    }}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="destination">Price</label>
+                  <input
+                    type="number"
+                    value={eventDetail.eventPrice}
+                    onChange={(e) => {
+                      setEventDetail({
+                        ...eventDetail,
+                        eventPrice: e.target.value,
+                      });
+                    }}
+                  />
+                </div>
+              </div>
+              <button onClick={createEventHandler}>Create</button>
             </div>
           </div>
-          <button onClick={createEventHandler}>Create</button>
-        </div>
-      </div>
+        </>
+      ) : (
+        <>
+          <Loading />
+        </>
+      )}
     </>
   );
 };
--- frontend/src/components/AdminDashBoard/Extras/HotelHandle.jsx
+++ frontend/src/components/AdminDashBoard/Extras/HotelHandle.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "../NavBar/AdminNavBar";
 import { toast } from "react-toastify";
 import "./extras.scss";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Loading from "../../Loading/Loading";
 const HotelHandle = () => {
   const [eventDetail, setEventDetail] = useState({
     name: "",
@@ -10,6 +12,8 @@ const HotelHandle = () => {
     roomPrice: 0,
     roomsLeft: 0,
   });
+  const [user, setUser] = useState(null);
+  const [role, setRole] = useState("");
 
   const createHotelHandler = async () => {
     if (eventDetail.name == "" || eventDetail.destination == "") {
@@ -41,68 +45,102 @@ const HotelHandle = () => {
     }
   };
 
+  const navigate = useNavigate();
+
+  const getUserDetails = async () => {
+    const { data } = await axios.get("/api/v1/me");
+    setUser(data);
+    setRole(data?.user?.role);
+  };
+
+  useEffect(() => {
+    getUserDetails();
+  });
+
+  useEffect(() => {
+    if (role == "user") {
+      toast.error("You are not Authorized !!");
+      navigate("/");
+    }
+    getUserDetails();
+  }, [role]);
+
+  useEffect(() => {
+    if (user?.success == false) {
+      toast.error("Unauthorized Access !!");
+      navigate("/");
+    }
+  }, [user]);
   return (
     <>
       <AdminNavBar />
 
-      <div className="content create-div-cont color-change">
-        <div className="create-div">
-          <h1>Add Hotel</h1>
+      {user != null && user?.success == true && role == "admin" ? (
+        <>
+          <div className="content create-div-cont color-change">
+            <div className="create-div">
+              <h1>Add Hotel</h1>
 
-          <div className="create-form">
-            <div>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                value={eventDetail.name}
-                onChange={(e) => {
-                  setEventDetail({ ...eventDetail, name: e.target.value });
-                }}
-              />
-            </div>
-            <div>
-              <label htmlFor="destination">Destination</label>
-              <input
-                type="text"
-                value={eventDetail.destination}
-                onChange={(e) => {
-                  setEventDetail({
-                    ...eventDetail,
-                    destination: e.target.value,
-                  });
-                }}
-              />
-            </div>
-            <div>
-              <label htmlFor="destination">Room Price</label>
-              <input
-                type="number"
-                value={eventDetail.roomPrice}
-                onChange={(e) => {
-                  setEventDetail({
-                    ...eventDetail,
-                    roomPrice: e.target.value,
-                  });
-                }}
-              />
-            </div>
-            <div>
-              <label htmlFor="destination">Rooms Left</label>
-              <input
-                type="number"
-                value={eventDetail.roomsLeft}
-                onChange={(e) => {
-                  setEventDetail({
-                    ...eventDetail,
-                    roomsLeft: e.target.value,
-                  });
-                }}
-              />
+              <div className="create-form">
+                <div>
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    value={eventDetail.name}
+                    onChange={(e) => {
+                      setEventDetail({ ...eventDetail, name: e.target.value });
+                    }}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="destination">Destination</label>
+                  <input
+                    type="text"
+                    value={eventDetail.destination}
+                    onChange={(e) => {
+                      setEventDetail({
+                        ...eventDetail,
+                        destination: e.target.value,
+                      });
+                    }}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="destination">Room Price</label>
+                  <input
+                    type="number"
+                    value={eventDetail.roomPrice}
+                    onChange={(e) => {
+                      setEventDetail({
+                        ...eventDetail,
+                        roomPrice: e.target.value,
+                      });
+                    }}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="destination">Rooms Left</label>
+                  <input
+                    type="number"
+                    value={eventDetail.roomsLeft}
+                    onChange={(e) => {
+                      setEventDetail({
+                        ...eventDetail,
+                        roomsLeft: e.target.value,
+                      });
+                    }}
+                  />
+                </div>
+              </div>
+              <button onClick={createHotelHandler}>Create</button>
             </div>
           </div>
-          <button onClick={createHotelHandler}>Create</button>
-        </div>
-      </div>
+        </>
+      ) : (
+        <>
+          <Loading />
+        </>
+      )}
     </>
   );
 };
